ff21bf2f49d0d5b1ffbbe28b133d4e5b1fa1f6ba
priyatam-mishra <148609633+priyatam-mishra@users.noreply.github.com>
2023-10-22 17:42:03 +0530

    p3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,38 @@
-a="<h1>lion</h1>"
-document.getElementById('jk').innerHTML = a
+a=5;
+b=3;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a||b);
+console.log(a&&b);
+console.log(a<<b);
+console.log(a>>b);
+console.log(a^b);
+
+console.log(a|b);
+console.log(a&b);
+console.log(a<b);
+console.log(a>b);
+
+console.log(a<=b);
+console.log(a>=b);
+console.log(a+=3);
+console.log(a-=7);
+
+console.log(a=+b);
+console.log(a=-b);
+console.log(!a);
+console.log(!b);
+
+a=10;
+ b=a--;
+ b=++a;
+ b=--b;
+
+console.log('a=',a);
+console.log('b=',b);
+
+
+
