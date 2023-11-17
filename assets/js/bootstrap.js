c26636c2587d3c73422f0cb1be780c82bd1bf270
priyatam-mishra <148609633+priyatam-mishra@users.noreply.github.com>
2023-11-17 08:30:17 +0530

    ppp

--- assets/js/bootstrap.js
+++ assets/js/bootstrap.js
@@ -9,3 +9,25 @@ document.querySelectorAll('.close')[0].addEventListener('click', () => {
    x.transform = 'translateX(-300px)'
    x.transition = 'all .8s'
 })
+
+document.querySelectorAll('.contact')[0].addEventListener('click', () => {
+   let x = document.querySelector('.sec_menu').style
+   x.transform = 'translateX(-300px)'
+   x.transition = 'all .8s'
+})
+
+document.querySelectorAll('.home')[0].addEventListener('click', () => {
+   let x = document.querySelector('.sec_menu').style
+   x.transform = 'translateX(-300px)'
+   x.transition = 'all .8s'
+})
+
+document.querySelectorAll('.membership')[0].addEventListener('click', () => {
+   let x = document.querySelector('.sec_menu').style
+   x.transform = 'translateX(-300px)'
+   x.transition = 'all .8s'
+})
+
+
+
+
